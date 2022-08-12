ab7f949b1d46c8ba7eeec8ddc1ce84c0eb8f9a17
Yahya <kh28699@gmail.com>
2022-08-12 18:57:55 +0300

    hello

--- app/src/main/java/com/example/zakahdeserved/actdelayentrystatisticalActivity.java
+++ app/src/main/java/com/example/zakahdeserved/actdelayentrystatisticalActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class actdelayentrystatisticalActivity extends AppCompatActivity {
     int s;
-    int g;
+    int gf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
