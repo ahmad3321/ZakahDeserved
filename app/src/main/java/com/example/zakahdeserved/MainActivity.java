2e80502065e255a9d2a79ab94edbca1195e6322b
PC <hasoyaser8@gmail.com>
2022-08-12 19:01:15 +0300

    dsfgs

--- app/src/main/java/com/example/zakahdeserved/MainActivity.java
+++ app/src/main/java/com/example/zakahdeserved/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     String s;
     int x = 10;
+    int y = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
