74630efbef5f89b5e21716251fb9afd6d42618db
PC <hasoyaser8@gmail.com>
2022-08-12 16:16:36 +0300

    main Activity

--- app/src/main/java/com/example/zakahdeserved/MainActivity.java
+++ app/src/main/java/com/example/zakahdeserved/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    String name = "test name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
