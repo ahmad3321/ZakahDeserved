c85cae7e0db295d0e7a5f859a3e6f2219fd7db4e
Yahya <kh28699@gmail.com>
2022-08-14 00:54:30 +0300

    hello

---
+++ app/src/main/java/com/example/zakahdeserved/ChildFragment/Tab1.java
@@ -0,0 +1,31 @@
+package com.example.zakahdeserved.ChildFragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.zakahdeserved.R;
+
+
+public class Tab1 extends Fragment {
+
+    public Tab1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_tab1, container, false);
+    }
+
+}
