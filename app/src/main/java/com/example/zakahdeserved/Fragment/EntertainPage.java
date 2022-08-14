e49a7e1d01f9e749a4c849bc6217b324e6e39dd5
Yahya <kh28699@gmail.com>
2022-08-14 18:43:16 +0300

    hello

---
+++ app/src/main/java/com/example/zakahdeserved/Fragment/EntertainPage.java
@@ -0,0 +1,99 @@
+package com.example.zakahdeserved.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.zakahdeserved.ChildFragment.Tab1;
+import com.example.zakahdeserved.ChildFragment.Tab2;
+import com.example.zakahdeserved.ChildFragment.Tab3;
+import com.example.zakahdeserved.ChildFragment.Tab4;
+import com.example.zakahdeserved.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EntertainPage extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private FragmentActivity myContext;
+
+    public EntertainPage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.entertain_page, null);
+
+        viewPager = (ViewPager)v.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout)v.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        return v;
+
+
+    }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new Tab1(), "ForU");
+        adapter.addFragment(new Tab2(), "Webseries");
+        adapter.addFragment(new Tab3(), "Trailer");
+        adapter.addFragment(new Tab4(), "Shortflim");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/zakahdeserved/Fragment/MusicPage.java
@@ -0,0 +1,31 @@
+package com.example.zakahdeserved.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.zakahdeserved.R;
+
+
+public class MusicPage extends Fragment {
+
+    public MusicPage() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.music_page, container, false);
+    }
+
+}
