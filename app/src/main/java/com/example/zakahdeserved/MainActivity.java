086dc4646b67578a7a06f91330edf7fa8e3d9d4e
Yahya <kh28699@gmail.com>
2022-08-16 10:29:00 +0300

    hello

--- app/src/main/java/com/example/zakahdeserved/MainActivity.java
+++ app/src/main/java/com/example/zakahdeserved/MainActivity.java
@@ -3,15 +3,40 @@ package com.example.zakahdeserved;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
-    String s;
-    int x = 10;
-    int y = 10;
+    TextView login;
+    EditText username,password;
+    ProgressBar progressBar;
+    // End Declaring layout button, edit texts
 
+    // Declaring connection variables
+    Connection con;
+    String un,pass,db,ip;
+    //End Declaring connection variables
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting values from button, texts and progress bar
+        login = (TextView) findViewById(R.id.btn_login);
+        username = (EditText) findViewById(R.id.edit_username);
+        password = (EditText) findViewById(R.id.edit_password);
+        //progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        //progressBar.setVisibility(View.GONE);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
