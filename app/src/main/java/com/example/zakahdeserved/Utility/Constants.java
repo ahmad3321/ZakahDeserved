17bc2ea16846a13f1c9698f23823ec7ec63ed281
CoderDevMe <97431541+CoderDevMe@users.noreply.github.com>
2022-08-30 16:23:26 +0300

    main Activity

--- app/src/main/java/com/example/zakahdeserved/Utility/Constants.java
+++ app/src/main/java/com/example/zakahdeserved/Utility/Constants.java
@@ -8,7 +8,7 @@ import com.example.zakahdeserved.Connection.SQLiteDAL;
 
 public class Constants {
     public static final int STATISTICAL_JOB_TITLE = 0;
-    public static final int DISTRIBUTION_JOB_TITLE = 1;
+    public static final int DISTRIBUTION_JOcB_TITLE = 1;
     public static SQLiteDAL SQLITEDAL;
     public static MasterKey SHAREDPREFERENCES_KEY;
     public static View view1, view2, view3, view4, view5, view6;
