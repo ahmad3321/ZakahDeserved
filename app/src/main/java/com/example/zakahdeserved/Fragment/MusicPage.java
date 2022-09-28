35d8e50a911eea3d77d0d3a1645c26749544443f
Yahya <kh28699@gmail.com>
2022-09-28 12:48:08 +0300

    hello

--- app/src/main/java/com/example/zakahdeserved/Fragment/MusicPage.java
+++ app/src/main/java/com/example/zakahdeserved/Fragment/MusicPage.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -15,7 +16,7 @@ import com.example.zakahdeserved.ChildFragment.Programms_Tab1;
 import com.example.zakahdeserved.ChildFragment.Programms_Tab2Fidaa;
 import com.example.zakahdeserved.ChildFragment.Programms_Tab3hayat;
 import com.example.zakahdeserved.ChildFragment.Programms_Tab4amal;
-
+import com.example.zakahdeserved.ChildFragment.Programms_Tab5Student;
 import com.example.zakahdeserved.R;
 import com.google.android.material.tabs.TabLayout;
 
@@ -42,19 +43,51 @@ public class MusicPage extends Fragment {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.music_page, container, false);
         viewpager_programms = (ViewPager)v.findViewById(R.id.viewpager_programms);
-        setupViewPager(viewpager_programms);
-
+        setupViewPager(viewpager_programms,"KTLAL");
         tabLayout = (TabLayout)v.findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewpager_programms);
+        setProgramTabs("");
         return v;
     }
-    private void setupViewPager(ViewPager viewPager) {
+    private void setupViewPager(ViewPager viewPager,String Programm) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
-        adapter.addFragment(new Programms_Tab1(), "البرامج");
+        switch (Programm){
+            case "KTLAL":
+                adapter.addFragment(new Programms_Tab1(), "البرامج");
+            case "Fidaa":
+                adapter.addFragment(new Programms_Tab2Fidaa(), "فداء");
+            case "Hayat":
+                adapter.addFragment(new Programms_Tab3hayat(), "حياة");
+            case "Amal":
+                adapter.addFragment(new Programms_Tab4amal(), "أمل");
+            case "Student":
+                adapter.addFragment(new Programms_Tab5Student(), "وقل ربي زدني علما");
+        }
+        /*adapter.addFragment(new Programms_Tab1(), "البرامج");
         adapter.addFragment(new Programms_Tab2Fidaa(), "فداء");
         adapter.addFragment(new Programms_Tab3hayat(), "حياة");
         adapter.addFragment(new Programms_Tab4amal(), "أمل");
+        adapter.addFragment(new Programms_Tab5Student(), "وقل ربي زدني علما");*/
         viewPager.setAdapter(adapter);
+    }
+    private void setProgramTabs(String Program) {
+        ((LinearLayout) tabLayout.getTabAt(0).view).setVisibility(View.GONE);
+        ((LinearLayout) tabLayout.getTabAt(1).view).setVisibility(View.GONE);
+        ((LinearLayout) tabLayout.getTabAt(2).view).setVisibility(View.GONE);
+        ((LinearLayout) tabLayout.getTabAt(3).view).setVisibility(View.GONE);
+        ((LinearLayout) tabLayout.getTabAt(4).view).setVisibility(View.GONE);
+        switch (Program){
+            case "KTLAL":
+                ((LinearLayout) tabLayout.getTabAt(0).view).setVisibility(View.VISIBLE);
+            case "Fidaa":
+                ((LinearLayout) tabLayout.getTabAt(1).view).setVisibility(View.VISIBLE);
+            case "Hayat":
+                ((LinearLayout) tabLayout.getTabAt(2).view).setVisibility(View.VISIBLE);
+            case "Amal":
+                ((LinearLayout) tabLayout.getTabAt(3).view).setVisibility(View.VISIBLE);
+            case "Student":
+                ((LinearLayout) tabLayout.getTabAt(4).view).setVisibility(View.VISIBLE);
+        }
     }
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
